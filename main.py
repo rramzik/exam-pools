730ed1fb748c782cedadc4d5618d9b6d86952917
rramzik <redhat478@gmail.com>
2022-12-06 19:39:39 +0700

    fix encoding

--- main.py
+++ main.py
@@ -7,14 +7,17 @@ def create_exam(fname):
     for patch in arr:
         ddd = os.listdir('pools/'+patch)
         file_name = random.choice(ddd)
-        f = open("pools/"+patch+"/"+file_name,"r")
-        exam.write(f.read())
+        f = open("pools/"+patch+"/"+file_name,"r",encoding='utf-8')
+        read = f.read()
+        exam.write(read)
         exam.write("\n")
         exam.write("\n")
     exam.close()
 
 j=int(input("Количество билетов "))
+
 for i in range(j):
     create_exam("exam"+str(i)+".txt")
+    print(i)
 print("Билеты ищите в папке exam")
-print("Перед повторным созданием билетов удалите/переместите старые5")
+print("Перед повторным созданием билетов удалите/переместите старые")
